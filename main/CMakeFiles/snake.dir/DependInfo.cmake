5d1f3f6cb55479263ecca55e3bb1c0a4ecd6b2b5
Gaelle MANGIN <gmangin@e2r11p23.42.fr>
2015-03-02 17:12:32 +0100

    start cmake

---
+++ main/CMakeFiles/snake.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/nfs/zfs-student-2/users/2013/gmangin/Nibbler/main/src/main.cpp" "/nfs/zfs-student-2/users/2013/gmangin/Nibbler/main/CMakeFiles/snake.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ main/CMakeFiles/snake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake.dir/src/main.cpp.o"
+  "snake.pdb"
+  "snake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
