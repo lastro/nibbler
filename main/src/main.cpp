177e540d147e2b5bcf17362052049a274da0cf3f
Gaelle MANGIN <gmangin@e3r2p13.42.fr>
2015-03-04 20:01:46 +0100

    game init (map and snake random place and 1 food random)

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -6,7 +6,7 @@
 //   By: gmangin <gaelle.mangin@hotmail.fr>                                   //
 //                                                                            //
 //   Created: 2015/03/02 12:39:27 by gmangin                                  //
-//   Updated: 2015/03/04 19:10:31 by gmangin          ###   ########.fr       //
+//   Updated: 2015/03/04 20:00:51 by gmangin          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -14,6 +14,60 @@
 #include	<cstdlib>
 #include	<signal.h>
 #include	<dlfcn.h>
+#include    <vector>
+#include	<ctime>
+
+void	display_map(std::vector< std::vector <int > > &map, int x, int y)
+{
+	for (int i = 0; i < y; ++i)
+    {
+        for (int j = 0; j < x; j++)
+        {
+			std::cout << map[i][j] << " ";
+        }
+		std::cout << std::endl;
+    }
+}
+
+// partie INIT du snake sur la map a un endroit random (map rempli
+// de 1 TETE et 1 QUEUE), le snake fait au debut 2 cases du tableau
+void    init_snake(std::vector< std::vector <int > > &map, int x, int y)
+{
+	int	i;
+	int	j;
+
+	// je place la tete de maniere random
+	i = std::rand() % (y - 4) + 1;
+	j = std::rand() % (x - 2) + 1;
+	std::cout << i << " " << j << std::endl;
+	map[i][j] = 1;
+	// puis je place la queue derriere :)
+	map[i - 1][j] = 2;
+}
+
+// partie INIT de la map (double tableau rempli de 0 == empty)
+void    init_map(std::vector< std::vector <int > > &map, int x, int y)
+{
+    map.resize(y);
+    for (int i = 0; i < y; ++i)
+    {
+        map[i].resize(x);
+        for (int j = 0; j < x; j++)
+        {
+            map[i][j] = 0;
+        }
+    }
+}
+
+// le jeu est lance, on place les pions et on les affiches dans les diff lib.
+void    start_game(Element *game)
+{
+	std::vector< std::vector <int > > map;
+
+    init_map(map, game->get_x(), game->get_y());
+    init_snake(map, game->get_x(), game->get_y());
+	display_map(map, game->get_x(), game->get_y());
+}
 
 // function qui check les options mis en argument
 int	check_arg(int argc, char **argv)
@@ -26,10 +80,13 @@ int	check_arg(int argc, char **argv)
     }
 	else if (argc == 3)
     {
-		if (std::atoi(argv[1]) == 0 || std::atoi(argv[2]) == 0)
+        if (std::atoi(argv[1]) < 5 || std::atoi(argv[2]) < 5)
 		{
-			std::cout << "usage: snake [width] [height]" << std::endl;
-			return (1);
+            if (std::atoi(argv[1]) == 0 || std::atoi(argv[2]) == 0)
+				std::cout << "usage: snake [width] [height]" << std::endl;
+            else
+				std::cout << "usage: the minimum of the width and the height is 5 " << std::endl;
+            return (1);
 		}
 		std::cout << "the width is " << std::atoi(argv[1]);
 		std::cout << " and the height is " << std::atoi(argv[2]) << std::endl;
@@ -81,12 +138,14 @@ int		main(int argc, char **argv)
     Element *game;
 
     game = new Element();
+	srand(static_cast<unsigned int>(time( NULL )));
 
 	if (check_arg(argc, argv))
 		return (1);
 	try
 	{
         set_window(argc, argv, game);
+		start_game(game);
 	}
 	catch(std::string const& chaine)
 	{
