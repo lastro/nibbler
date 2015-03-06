ad06e680cdb80da38cdcc78039701c724dad069b
ExeQ <musicantoni@gmail.com>
2015-03-06 16:02:55 +0100

    modif install.sh et CmakeLists: ajout de sdl2

--- install.sh
+++ install.sh
@@ -3,19 +3,20 @@ echo "Brew Update";
 brew update;
 echo "Done";
 
-echo "brew install boost";
-brew install boost;
+echo "brew install SDL2";
+brew install sdl2;
+
+echo "brew install Qt";
+brew install Qt;
 
 echo "cmake . -G 'Unix Makefiles'"
 cmake . -G "Unix Makefiles";
 
 echo "Setting variables in CMakeCache.txt"
-echo "BOOST_INCLUDEDIR:FILEPATH="$HOME"/.brew/Cellar/boost/1.57.0/include" >> CMakeCache.txt;
-echo "BOOST_LIBRARYDIR:FILEPATH="$HOME"/.brew/Cellar/boost/1.57.0/lib" >> CMakeCache.txt;
-echo "BOOST_ROOT:FILEPATH="$HOME"/.brew/Cellar/boost" >> CMakeCache.txt;
 
-echo "cmake . -G 'Unix Makefiles' with new variables"
-cmake . -G "Unix Makefiles";
+echo "SDL2_INCLUDEDIR:FILEPATH="$HOME"/.brew/Cellar/sdl2/2.0.3/include/SDL2" >> CMakeCache.txt;
+echo "SDL2_LIBRARYDIR:FILEPATH="$HOME"/.brew/Cellar/sdl2/2.0.3/lib" >> CMakeCache.txt;
+echo "SDL2_ROOT:FILEPATH="$HOME"/.brew/Cellar/sdl2" >> CMakeCache.txt;
 
 echo "make project"
 make;
